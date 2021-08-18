801236fd31cda2426252489637c859506cd83ac4
Torianna Hudson <toriannahudson@quickenloans.comclear>
2021-08-18 10:15:24 -0400

    last

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OddEvenPractice
+{
+    public class Numbers
+    {
+        public static string EvenNumbers(int num)
+        {
+
+            if (num % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+
+
+
+        }
+    }
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Numbers.EvenNumbers(2));
+           for (int i=1; i<=100; i++)
+            {
+                Console.WriteLine($"{i} {Numbers.EvenNumbers(i)}");
+            }
+           
+            
+        }
+    }
+}
